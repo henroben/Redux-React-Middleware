fe22ecf686e80ee78129f1329a1e9731006174fa
Ben Mercer <ben@henroben.co.uk>
2017-03-14 15:48:53 +0000

    Static action creator

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import { FETCH_USERS } from 'types';
+
+export function fetchUsers() {
+    return {
+        type: FETCH_USERS,
+        payload: [
+            { name: 'Ben' },
+            { name: 'Luke' },
+            { name: 'Nuno' },
+            { name: 'Mark' },
+            { name: 'Derek' },
+            { name: 'Peter' }
+        ]
+    }
+}
